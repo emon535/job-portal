57852811a85651a83d16557d57103345ad0d2b2c
emon535 <emon535@gmail.com>
2024-06-08 11:09:26 +0100

    added fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import store from '../store';
 
 const api = axios.create({
   baseURL: 'https://tasker.mubeendroid.com', 
 });
 
 api.interceptors.request.use((config) => {
-  const state = store.getState();
   const token = 'asnerkvehrvewjd%&23rdv!FL*scbweuf&^687wbeHGrmd^&ej67^&*bc5eF%!6XC3E'
   if (token) {
     config.headers.Authorization = `${token}`;
